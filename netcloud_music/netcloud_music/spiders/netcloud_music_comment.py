35a6f0dd300afb40ff79ea0e2ee0a9d320905988
miaoqiao <miaoqiao0517@163.com>
2018-12-28 23:36:04 +0800

    get net_cloud_music songs href list

--- netcloud_music/netcloud_music/spiders/netcloud_music_comment.py
+++ netcloud_music/netcloud_music/spiders/netcloud_music_comment.py
@@ -6,6 +6,11 @@ class NetcloudMusicCommentSpider(scrapy.Spider):
     name = 'netcloud_music_comment'
     allowed_domains = ['music.163.com/#/discover/toplist?id=3778678']
     start_urls = ['http://music.163.com/#/discover/toplist?id=3778678/']
+    index = 1
 
     def parse(self, response):
-        pass
+        print(response.css('div.ttt'))
+        for song in response.css('div.ttt'):
+            song_page = song.css('a::attr(href)').extract_first()
+            print(self.index + '------' + song_page)
+            self.index += 1
